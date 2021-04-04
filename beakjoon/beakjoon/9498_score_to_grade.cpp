100ab74cf9f2857f9185586397d4f265d8ac3219
duswl8811 <duswlsp@naver.com>
2021-04-04 23:32:03 +0900

    단계별 2단계(3/5)

---
+++ beakjoon/beakjoon/9498_score_to_grade.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int A = 0;
+
+	std::cin >> A;
+
+	if (A >= 90) std::cout << "A" << std::endl;
+	else if (A >= 80) std::cout << "B" << std::endl;
+	else if (A >= 70) std::cout << "C" << std::endl;
+	else if (A >= 60) std::cout << "D" << std::endl;
+	else { std::cout << "F" << std::endl; };
+
+	return 0;
+}

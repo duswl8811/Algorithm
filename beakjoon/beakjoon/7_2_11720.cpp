486ae182ad6ef5e71f548ee97f416feb39f67030
duswl8811 <duswlsp@naver.com>
2021-04-17 23:47:09 +0900

    6단계 fix
    문자열 파트라 문자열로 풀고 싶었는데 대부분 그냥 문자열을 안쓰고 푸는 것 같다...

--- beakjoon/beakjoon/7_2_11720.cpp
+++ beakjoon/beakjoon/7_2_11720.cpp
@@ -2,15 +2,19 @@
 
 int main()
 {
-	char arr[101] = { 0 };
+	//char arr[101] = { 0 };
+	char a = 0;
 	int n = 0, sum = 0;
-	std::cin >> n; // 1<=N<=100
-	std::cin.ignore();
+	std::cin >> n;
+	//std::cin.ignore();
 
-	std::cin.getline(arr, 102);
+	//std::cin.getline(arr, 102);
 
-	while (n--)
-		sum += arr[n] - '0';
+	while (n--) {
+		std::cin >> a;
+		//sum += arr[n] - '0';
+		sum += a - '0';
+	}
 	
 	std::cout << sum << std::endl;
 }

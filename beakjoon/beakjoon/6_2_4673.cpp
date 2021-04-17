11b120a3af879b584559eb754193a3b17b0a35e4
duswl8811 <duswlsp@naver.com>
2021-04-18 04:48:55 +0900

    단계별 6_2

--- beakjoon/beakjoon/6_2_4673.cpp
+++ beakjoon/beakjoon/6_2_4673.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 
+int d(int n);
+
 int main()
 {
-	int n = 0;
-	std::cin >> n;
+	char selfnum_arr[10001] = { 0 };
 
-	std::cout << n << std::endl;
+	for (int i = 1; i < 10001; i++) {
+		int k = d(i);
+		if (k < 10001)
+			selfnum_arr[k] = true;
+	}
+	for (int i = 1; i < 10001; i++) 
+		if (!selfnum_arr[i])
+			std::cout << i << "\n";
+}
+
+int d(int n)
+{
+	int sum = n;
+
+	while (n>0)
+	{
+		sum += n % 10;
+		n /= 10;
+	}
+	return sum;
 }

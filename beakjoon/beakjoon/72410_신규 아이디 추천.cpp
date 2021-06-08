22675af1dad7caa5b4afd7b11ce2fb2e766650f6
duswl8811 <duswlsp@naver.com>
2021-06-08 20:19:09 +0900

    프로그래머스 신규 아이디 추천(1)
    모든 test case가 통과 되는 코드
    필요 없는 부분 정리 필요

--- beakjoon/beakjoon/72410_신규 아이디 추천.cpp
+++ beakjoon/beakjoon/72410_신규 아이디 추천.cpp
@@ -1,5 +1,7 @@
 // 2021-06-08 16:40 - 19:00		only testcase:5 is no-pass;
+// 2021-06-08 19:30 - 20:15
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <vector>
 
@@ -12,36 +14,35 @@ string solution(string new_id) {
 	// "...!@BaT#*..y.abcdefghijklm"	-> "bat.y.abcdefghi"
 	// 1. 대문자 -> 소문자
 	// "...!@BaT#*..y.abcdefghijklm"	-> "...!@bat#*..y.abcdefghijklm"
+	//for (int i = 0; i<answer.size(); i++)
+	//	answer[i] = (char)tolower(answer[i]);
+	transform(answer.begin(), answer.end(), answer.begin(), ::tolower);
 	// 2. 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)만 허가
 	// "...!@bat#*..y.abcdefghijklm"	-> "...bat..y.abcdefghijklm"
+	for (int i = 0; i < answer.size(); ++i)
+	{
+		if (islower(answer[i]) || isdigit(answer[i]) || data.find(answer[i]) != string::npos)
+			continue;
+
+		else
+		{
+			answer.erase(i, 1);
+			--i;
+		}
+	}
 	// 3. ".." -> "."
 	// "...bat..y.abcdefghijklm"		-> ".bat.y.abcdefghijklm"
+	for (int i = 1; i < answer.size(); i++)
+	{
+		if (answer[i] == '.')
+			if (answer[i + 1] == '.' || answer[i - 1] == '.')
+				answer.erase(i--, 1);
+	}
+
 	// 4. front & back != '.'
 	// ".bat.y.abcdefghijklm"			-> "bat.y.abcdefghijklm"
-	// 5. front == '\0' -> front = 'a'
-	// no change 
-	// 6. if 15 < size() then next delete
-	// "bat.y.abcdefghijklm"			-> "bat.y.abcdefghi"
-	// 7. if size() < 3 then back + back until 3 == size() 
-	// no change 
-
-	// 1 ~ 4
-	for(int i = 0; i < answer.size();)
+	for (int i = 0; i < answer.size();)
 	{
-		if (isalpha(answer[i]))
-		{
-			answer[i] = (char)tolower(answer[i]);
-			++i;
-			continue;
-		}
-
-		if (isdigit(answer[i]))
-		{
-			++i;
-			continue;
-		}
-
-		//string::size_type n;
 		if (answer[i] == '.')
 		{
 			if (i == 0)
@@ -50,34 +51,83 @@ string solution(string new_id) {
 				continue;
 			}
 
-			if (answer[i + 1] == '.' || answer[i - 1] == '.')
-			{
-				answer.erase(i--, 1);
-				if (i == answer.size())
-				{
-					--i;
-					continue;
-				}
-			}
-			
 			if (i == answer.size() - 1)
 			{
-				answer.erase(i--, 1);
+				answer.erase(i, 1);
+				--i;
+				continue;
 			}
 			++i;
-			continue;
 		}
-
-		if (data.find(answer[i]) == string::npos)
-		{
-			answer.erase(i, 1);
-			if (answer[i] == '\0')
-				--i;
-			continue;
-		}
-
-		i++;
+		else
+			++i;
 	}
+
+	// 5. front == '\0' -> front = 'a'
+	// no change 
+
+
+	// 6. if 15 < size() then next delete
+	// "bat.y.abcdefghijklm"			-> "bat.y.abcdefghi"
+
+
+	// 7. if size() < 3 then back + back until 3 == size() 
+	// no change 
+
+
+	// 1 ~ 4
+	//for(int i = 0; i < answer.size();)
+	//{
+	//	if (isalpha(answer[i]))
+	//	{
+	//		answer[i] = (char)tolower(answer[i]);
+	//		++i;
+	//		continue;
+	//	}
+
+	//	if (isdigit(answer[i]))
+	//	{
+	//		++i;
+	//		continue;
+	//	}
+
+	//	//string::size_type n;
+	//	if (answer[i] == '.')
+	//	{
+	//		if (i == 0)
+	//		{
+	//			answer.erase(i, 1);
+	//			continue;
+	//		}
+
+	//		if (answer[i + 1] == '.' || answer[i - 1] == '.')
+	//		{
+	//			answer.erase(i--, 1);
+	//			if (i == answer.size())
+	//			{
+	//				--i;
+	//				continue;
+	//			}
+	//		}
+	//		
+	//		if (i == answer.size() - 1)
+	//		{
+	//			answer.erase(i--, 1);
+	//		}
+	//		++i;
+	//		continue;
+	//	}
+
+	//	if (data.find(answer[i]) == string::npos)
+	//	{
+	//		answer.erase(i, 1);
+	//		if (answer[i] == '\0')
+	//			--i;
+	//		continue;
+	//	}
+
+	//	i++;
+	//}
 
 	// 5 ~ 7
 	for (;;)
@@ -109,7 +159,7 @@ string solution(string new_id) {
 
 int main()
 {
-	string new_id = { ".aa-." };
+	string new_id = { "b......@" };
 
 	cout << solution(new_id);
 }
